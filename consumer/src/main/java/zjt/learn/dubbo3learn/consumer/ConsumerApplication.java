e8b04fa089b71ee44e2093b5c80b8d323ce98c9a
jtzhao <jtzhao@ichint.com>
2022-07-19 17:49:39 +0800

    去掉dubbo配置

--- consumer/src/main/java/zjt/learn/dubbo3learn/consumer/ConsumerApplication.java
+++ consumer/src/main/java/zjt/learn/dubbo3learn/consumer/ConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
